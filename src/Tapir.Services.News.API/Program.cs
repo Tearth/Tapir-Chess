e5b0ce132b1f5dfe01c55821f31e9af706153284
Tearth <tearthdev@gmail.com>
2025-03-16 19:09:21 +0100

    Add Swagger

--- src/Tapir.Services.News.API/Program.cs
+++ src/Tapir.Services.News.API/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.OpenApi.Models;
 using Tapir.Services.News.Application;
 using Tapir.Services.News.Infrastructure;
 
@@ -9,15 +10,23 @@ namespace Tapir.Services.News.API
         {
             var builder = WebApplication.CreateBuilder(args);
             builder.Services.AddControllers();
+            builder.Services.AddHttpContextAccessor();
             builder.Services.AddInfrastructure(builder.Configuration);
             builder.Services.AddApplication(builder.Configuration);
-            
+
+            builder.Services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
+            });
+
             var app = builder.Build();
             await app.UseInfrastructure();
             await app.UseApplication();
             app.UseHttpsRedirection();
             app.UseAuthorization();
             app.MapControllers();
+            app.UseSwagger();
+            app.UseSwaggerUI();
             app.Run();
         }
     }
