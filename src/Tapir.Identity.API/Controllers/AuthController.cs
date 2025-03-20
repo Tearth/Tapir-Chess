a8b951ee0eda886d7579bcfb8a1a8329844aab16
Tearth <tearthdev@gmail.com>
2025-03-20 19:28:37 +0100

    Add identity projects

---
+++ src/Tapir.Identity.API/Controllers/AuthController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Tapir.Identity.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+
+    }
+}
---
+++ src/Tapir.Identity.API/Program.cs
@@ -0,0 +1,16 @@
+namespace Tapir.Identity.API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddControllers();
+
+            var app = builder.Build();
+            app.UseAuthorization();
+            app.MapControllers();
+            app.Run();
+        }
+    }
+}
