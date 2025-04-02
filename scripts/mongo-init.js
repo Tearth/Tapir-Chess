0e07e0b563175c942e10fc93bbd5a3dc989ca271
Tearth <tearthdev@gmail.com>
2025-04-02 20:26:07 +0200

    Refactoring

--- scripts/mongo-init.js
+++ scripts/mongo-init.js
@@ -1,23 +1,15 @@
-db = db.getSiblingDB('news');
-db.createUser({
-    user: "tapir",
-    pwd: "JnhILirAtXrwILs",
-    roles: [{
-        role: "readWrite",
-        db: "news"
-    }]
-});
-db.events.createIndex({ "AggregateId": 1 });
-db.events.createIndex({ "Timestamp": 1 });
+let databases = ['news', 'players'];
 
-db = db.getSiblingDB('players');
-db.createUser({
-    user: "tapir",
-    pwd: "JnhILirAtXrwILs",
-    roles: [{
-        role: "readWrite",
-        db: "players"
-    }]
+databases.forEach((name) => {
+    db = db.getSiblingDB(name);
+    db.createUser({
+        user: "tapir",
+        pwd: "JnhILirAtXrwILs",
+        roles: [{
+            role: "readWrite",
+            db: name
+        }]
+    });
+    db.events.createIndex({ "AggregateId": 1 });
+    db.events.createIndex({ "Timestamp": 1 });
 });
-db.events.createIndex({ "AggregateId": 1 });
-db.events.createIndex({ "Timestamp": 1 });
