42bcb65659cab69af6b5fc8fc108a5fab5bafbea
Tearth <tearthdev@gmail.com>
2025-04-12 14:41:08 +0200

    Improve creation tests

--- test/Tapir.Services.News.Tests/Unit/News/NewsCreationTests.cs
+++ test/Tapir.Services.News.Tests/Unit/News/NewsCreationTests.cs
@@ -8,12 +8,16 @@ namespace Tapir.Services.News.Tests.Unit.News
         [Test]
         public void CreateNews_ValidNews_EventEmitted()
         {
-            // Arrange & Act
-            var newsEntity = new NewsEntity(Guid.NewGuid());
+            // Arrange
+            var id = Guid.NewGuid();
+
+            // Act
+            var newsEntity = new NewsEntity(id);
 
             // Assert
             Assert.Multiple(() =>
             {
+                Assert.That(newsEntity.Id, Is.EqualTo(id));
                 Assert.That(newsEntity.GetUncommittedEvents().Count, Is.EqualTo(1));
                 Assert.That(newsEntity.GetUncommittedEvents<NewsCreatedEvent>().Count, Is.EqualTo(1));
             });
--- test/Tapir.Services.Players.Tests/Unit/Players/PlayerCreationTests.cs
+++ test/Tapir.Services.Players.Tests/Unit/Players/PlayerCreationTests.cs
@@ -1,4 +1,5 @@
-﻿using Tapir.Services.Players.Domain.Players.Entities;
+﻿using System.Diagnostics.Metrics;
+using Tapir.Services.Players.Domain.Players.Entities;
 using Tapir.Services.Players.Domain.Players.Events;
 
 namespace Tapir.Services.Players.Tests.Unit.Players
@@ -8,12 +9,18 @@ namespace Tapir.Services.Players.Tests.Unit.Players
         [Test]
         public void CreatePlayer_ValidPlayer_EventEmitted()
         {
-            // Arrange & Act
-            var newsEntity = new PlayerEntity(Guid.NewGuid(), Guid.NewGuid());
+            // Arrange
+            var id = Guid.NewGuid();
+            var userId = Guid.NewGuid();
+
+            // Act
+            var newsEntity = new PlayerEntity(id, userId);
 
             // Assert
             Assert.Multiple(() =>
             {
+                Assert.That(newsEntity.Id, Is.EqualTo(id));
+                Assert.That(newsEntity.UserId, Is.EqualTo(userId));
                 Assert.That(newsEntity.GetUncommittedEvents().Count, Is.EqualTo(1));
                 Assert.That(newsEntity.GetUncommittedEvents<PlayerCreatedEvent>().Count, Is.EqualTo(1));
             });
