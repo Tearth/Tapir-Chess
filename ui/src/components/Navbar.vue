eb0bf2667a0e7564ffdafc806aa2478574c2dba0
Tearth <tearthdev@gmail.com>
2025-04-15 19:35:39 +0200

    Add Sign In view

---
+++ ui/src/components/Navbar.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="navbar bg-base-100 shadow-sm">
+    <div class="flex">
+      <a class="btn btn-ghost text-3xl pb-1">Tapir Chess</a>
+    </div>
+    <div class="flex-1 gap-2">
+      <div class="navbar-center">
+        <ul class="menu menu-horizontal text-lg">
+          <li><a>PLAYERS</a></li>
+          <li><a>GAMES</a></li>
+        </ul>
+      </div>
+    </div>
+    <div class="flex gap-2">
+      <div class="navbar-center">
+        <ul class="menu menu-horizontal text-lg">
+          <li><RouterLink to="/SignIn">SIGN IN</RouterLink></li>
+          <li><RouterLink to="/SignIn">JOIN US</RouterLink></li>
+        </ul>
+      </div>
+
+      <!--<div class="dropdown dropdown-end">
+          <div tabIndex={0} role="button" class="btn btn-ghost btn-circle avatar">
+            <div class="w-10 rounded-full">
+              <img
+                alt="Tailwind CSS Navbar component"
+                src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+            </div>
+          </div>
+          <ul
+            tabIndex={0}
+            class="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+            <li><a>Profile</a></li>
+            <li><a>Logout</a></li>
+          </ul>
+        </div> -->
+    </div>
+  </div>
+</template>
---
+++ ui/src/views/SignInView.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="flex flex-col items-center justify-center px-6 pt-16">
+    <form @submit.prevent="submitForm">
+      <fieldset class="fieldset w-xs bg-base-200 border border-base-300 p-4 rounded-box gap-3">
+        <legend class="fieldset-legend">Sign in</legend>
+
+        <label class="fieldset-label">Username</label>
+        <input v-model="username" class="input" />
+
+        <label class="fieldset-label">Password</label>
+        <input type="password" v-model="password" class="input" />
+
+        <div class="grid grid-cols-2 grid-rows-1 pt-1">
+          <div>
+            <label class="fieldset-label">
+              <input type="checkbox" v-model="rememberMe" defaultChecked class="checkbox" />
+              Remember me
+            </label>
+          </div>
+          <div class="text-right pt-1">
+            <RouterLink to="/reset-password" class="link link-neutral">Forgot password?</RouterLink>
+          </div>
+        </div>
+
+        <button type="submit" class="btn btn-neutral mt-4">Sign in</button>
+
+        <div class="text-center pt-2">
+          Don't have an account?
+          <RouterLink to="/register" class="link link-neutral">Join us!</RouterLink>
+        </div>
+      </fieldset>
+    </form>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  data() {
+    return {
+      username: '',
+      password: '',
+      rememberMe: false,
+    }
+  },
+  methods: {
+    async submitForm() {
+      alert(this.username + ' ' + this.password)
+    },
+  },
+}
+</script>
