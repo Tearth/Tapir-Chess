23966070c0f1bc28548279a8e1d88de9335126d5
Tearth <tearthdev@gmail.com>
2025-06-23 22:15:04 +0200

    Enable database monitoring

--- k8s/overlays/minikube/install.sh
+++ k8s/overlays/minikube/install.sh
@@ -1,6 +1,7 @@
 helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
 helm repo add bitnami https://charts.bitnami.com/bitnami
 helm repo add cnpg https://cloudnative-pg.github.io/charts
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo update
 
 kubectl create namespace ingress-nginx
@@ -8,8 +9,10 @@ kubectl create namespace postgresql
 kubectl create namespace mongodb
 kubectl create namespace rabbitmq
 kubectl create namespace services
+kubectl create namespace monitoring
 
 helm install ingress-nginx ingress-nginx/ingress-nginx --version 4.12.3 --values ../../base/values/nginx-values.yaml -n ingress-nginx
+helm install kube-prometheus prometheus-community/kube-prometheus-stack --values ../../base/values/kube-prometheus-values.yaml -n monitoring
 helm install postgresql cnpg/cloudnative-pg --values ../../base/values/postgresql-values.yaml --values ./values/postgresql-values.yaml -n postgresql
 helm install mongodb-games bitnami/mongodb --version 16.5.20 --values ../../base/values/mongodb-games-values.yaml --values ./values/mongodb-games-values.yaml -n mongodb
 helm install mongodb-news bitnami/mongodb --version 16.5.20 --values ../../base/values/mongodb-news-values.yaml --values ./values/mongodb-news-values.yaml -n mongodb
